aaf47689ad96780e4d1b5faac1d71060b6bd1eb6
MINHAJUL ABEDIN <mabedinfahim@gmail.com>
2022-02-25 20:10:31 +0600

    items null error handaling done

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ document.getElementById("search-btn").addEventListener("click", function() {
     const p = document.getElementById("p");
     const div = document.getElementById("row");
     if (isNaN(inputValue)) {
-        div.innerHTML = "";
+        div.textContent = "";
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
         fetch(url)
             .then(res => res.json())
-            .then(data => loadMeal(data.meals));
+            .then(data => {
+                console.log(data)
+                if (data.meals === null) {
+                    p.innerText = "No items are found"
+                } else {
+                    loadMeal(data.meals)
+                }
+            });
         const loadMeal = (meals) => {
             for (const meal of meals) {
                 const creatDiv = document.createElement("div");
@@ -20,13 +27,14 @@ document.getElementById("search-btn").addEventListener("click", function() {
                 const title = meal.strMeal;
                 const sTitle = title.slice(0, 20)
                 creatDiv.innerHTML = ` <div class="card mb-5">
-                        <img width="100px" height="200px"class="card-img-top" src="${meal.strMealThumb}" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">${sTitle}</h5>
-                            <p class="card-text">${pSlice}</p>
+                            <img width="100px" height="200px"class="card-img-top" src="${meal.strMealThumb}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">${sTitle}</h5>
+                                <p class="card-text">${pSlice}</p>
+                                <button onClick="loadDetails()" type="button" class="btn btn-outline-primary">Learn more</button>
+                            </div>
                         </div>
-                    </div>
-                    `
+                        `
                 div.appendChild(creatDiv);
             }
         }
@@ -61,6 +69,7 @@ const defaultFunction = (meals) => {
                 <div class="card-body">
                     <h5 class="card-title">${sTitle}</h5>
                     <p class="card-text">${pSlice}</p>
+                    <button type="button" class="btn btn-outline-primary">Learn more</button>
                 </div>
             </div>
             `
